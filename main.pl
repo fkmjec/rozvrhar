1fa04253f7ea1699c96604dc5a996d1f4fe42288
František Kmječ <frantisek.kmjec@gbl.cz>
2021-08-05 17:44:33 +0200

    Add normal date and time formats

--- main.pl
+++ main.pl
@@ -1,6 +1,24 @@
 %%%%%%%% SCHEDULE DATA %%%%%%%%
-subject(nswi177, 1, 2, s3).
-subject(nswi178, 2, 3, s3).
+subject(nswi177, date(mon, 14, 0), date(mon, 15, 30), s3).
+subject(nswi177, date(mon, 15, 40), date(mon, 17, 10), s4).
+subject(nswi178, date(mon, 14, 0), date(mon, 15, 30), s3).
+
+
+%%%%%%%% DATE AND TIME CONVERSION %%%%%%%%%
+daynum(mon, 0).
+daynum(tue, 1).
+daynum(wed, 2).
+daynum(thu, 3).
+daynum(fri, 4).
+daynum(sat, 5).
+daynum(sun, 6).
+
+% daytime2minutes(+date(Day, Hours, Mins), -Minutes)
+% convert time in the week into minutes since start of the week
+daytime2minutes(date(Day, Hours, Mins), Minutes) :- (
+    daynum(Day, DayNum),
+    Minutes is (DayNum * 1440 + Hours * 60 + Mins)
+).
 
 %%%%%%%% SCHEDULE VALIDATION %%%%%%%% 
 
@@ -14,8 +32,10 @@ compare_events(>, event(_, Time1), event(_, Time2)) :- (
 
 % create_events_acc(+Schedule, +Acc, -Events)
 create_events_acc([], Acc, Acc).
-create_events_acc([subject(_, StartTime, EndTime, _) | ScheduleSuffix], Acc, Events) :- (
-    create_events_acc(ScheduleSuffix, [event(start, StartTime), event(end, EndTime) | Acc], Events)
+create_events_acc([subject(_, Start, End, _) | ScheduleSuffix], Acc, Events) :- (
+    daytime2minutes(Start, StartMinutes),
+    daytime2minutes(End, EndMinutes),
+    create_events_acc(ScheduleSuffix, [event(start, StartMinutes), event(end, EndMinutes) | Acc], Events)
 ).
 
 % create_events(+Schedule, -SortedEvents)
@@ -53,8 +73,8 @@ is_schedule_feasible(Schedule) :- (
 % create_schedule_internal(+Codes, +Acc, -Schedule)
 create_schedule_internal([], Acc, Acc).
 create_schedule_internal([Code | CodesSuffix], Acc, Schedule) :- (
-    subject(Code, X, Y, Z),
-    create_schedule_internal(CodesSuffix, [subject(Code, X, Y, Z) | Acc], Schedule)
+    subject(Code, date(Day1, Hour1, Min1), date(Day2, Hour2, Min2), Place),
+    create_schedule_internal(CodesSuffix, [subject(Code, date(Day1, Hour1, Min1), date(Day2, Hour2, Min2), Place) | Acc], Schedule)
 ).
 
 % create_schedule(+SubjectCodes, -Schedule)
