5ec9e9a628a28da6be88e5500237d4237ddf357f
František Kmječ <frantisek.kmjec@gbl.cz>
2021-09-14 17:10:49 +0200

    Add input data, modify input parser

---
+++ data/input_parser.py
@@ -0,0 +1,62 @@
+import csv
+import argparse
+
+class Subject:
+    def __init__(self, name):
+        self.name = name
+        self.instances = {}
+    
+    def add_instance(self, instance):
+        self.instances[instance.type] = instance
+    
+    def print_instances(self):
+        pass
+
+class SubjectInstance:
+    def __init__(self, code, subject_type, start, end, teacher, building, room):
+        self.code = code
+        self.instance_type = instance_type
+        self.day = day
+        self.minutes = minutes
+        self.length = length
+        self.teacher = teacher
+        self.building = building
+        self.room = room
+    
+    def to_s(self):
+        return "subject_instance({}, {}, {}, {}, \'{}\', \'{}\', \'{}\', \'{}\').".format(
+            self.code,
+            self.instance_type,
+            self.start,
+            self.end,
+            self.teacher,
+            self.building,
+            self.room
+        )
+
+
+days = ['kek', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+def get_date(day, minutes):
+    return "date({}, {}, {})".format(days[day], minutes // 60, minutes % 60)
+
+def load_csv(filename):
+    subjects = {}        
+    with open(filename) as input_file:
+        reader = csv.reader(input_file, delimiter=';')
+        next(reader)
+        for row in reader:
+            if row[0] == '':
+                continue
+            elif row[2] != '' and row[3] != '' and  row[4] != '' and row[5] != '' and row[6] != '' and row[7] != '':
+                unique_code = row[0].lower()
+                code = row[2].lower()
+                name = row[3]
+                day = int(row[4])
+                minutes = int(row[5])
+                place = row[6].lower()
+                length = int(row[7])
+                date_start = get_date(day, minutes)
+                date_end = get_date(day, minutes + length)
+                teacher = 
+
