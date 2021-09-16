dbc592f2f1d89b66d691609f7b3fc693bdb69b97
František Kmječ <frantisek.kmjec@gbl.cz>
2021-09-16 08:27:28 +0200

    Clean up, add impossible test

---
+++ test_impossible.pl
@@ -0,0 +1,6 @@
+subject(nswi179, 'Úvod do Unixu 2', [x]).
+subject_instance(nswi179, x, date(tue, 15, 40), date(tue, 17, 10), 'Petr Tůma', s4, ms).
+subject(nswi178, 'Úvod do Unixu 101', [x]).
+subject_instance(nswi178, x, date(tue, 15, 50), date(tue, 17, 20), 'Vojtěch Horký', s4, ms).
+
+% when asked get_possible_schedules([nswi178, nswi179]), it should not output anything because of the collision 
